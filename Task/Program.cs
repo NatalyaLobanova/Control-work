b8be63c51aab3dcb7a04a39c2ef4dbe99ca1fdf8
Natalya Lobanova <knopka40a@mail.ru>
2022-09-20 21:10:53 +0500

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arrayA = {"Hello", "23", "world", ":-)", "152", "md"};
+string[] arrayB = new string[arrayA.Length]; 
+int count = 0;
+
+void SecondArray(string[] arrayA, string[] arrayB)
+{
+for (int i = 0; i < arrayA.Length; i++)
+    {  
+        if (arrayA[i].Length <= 3)
+        {
+            arrayB[count] = arrayA[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+     {
+        Console.Write(array[i] + " ");
+     }
+}
+SecondArray(arrayA, arrayB);
+PrintArray(arrayB);
